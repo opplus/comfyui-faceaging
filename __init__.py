c5ac95f193827bd8c2aa058abda8b259e0e71b79
oplus <oplus.wang@huanma.zone>
2024-11-05 14:27:05 +0800

    AgeOffloadModelNode

--- __init__.py
+++ __init__.py
@@ -1,18 +1,19 @@
 # custom_nodes/comfyui_faceaging/__init__.py
 
+import logging
 import os
 import warnings
-import logging
-import torch
-import torchvision.transforms as transforms
-import numpy as np
-import dlib
 from argparse import Namespace
 
-from .datasets.augmentations import AgeTransformer 
-from .utils.common import tensor2im  
-from .scripts.align_all_parallel import align_face
+import dlib
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+
+from .datasets.augmentations import AgeTransformer
 from .models.psp import pSp
+from .scripts.align_all_parallel import align_face
+from .utils.common import tensor2im
 
 # Suppress specific warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
@@ -32,44 +33,74 @@ CURRENT_DIR = 'models/face_aging/'
 PREDICTOR_PATH = os.path.join(CURRENT_DIR, "shape_predictor_68_face_landmarks.dat")
 MODEL_PATH = os.path.join(CURRENT_DIR, "sam_ffhq_aging.pt")
 
-# Check and load shape predictor
-if not os.path.exists(PREDICTOR_PATH):
-    raise FileNotFoundError(f"Shape predictor not found at {PREDICTOR_PATH}")
-predictor = dlib.shape_predictor(PREDICTOR_PATH)
+predictor = None
+net = None
+transform = None
 
-# Define model paths dictionary
-model_paths = {
-    'psp_model': MODEL_PATH
-}
 
-# Check and load model
-if not os.path.exists(MODEL_PATH):
-    raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
+def load_model():
+    global predictor
+    global net
+    global transform
+    # Check and load shape predictor
+    if not os.path.exists(PREDICTOR_PATH):
+        raise FileNotFoundError(f"Shape predictor not found at {PREDICTOR_PATH}")
+    predictor = dlib.shape_predictor(PREDICTOR_PATH)
 
-# Load checkpoint
-ckpt = torch.load(MODEL_PATH, map_location='cpu')  # weights_only 인자 제거
-opts = ckpt.get('opts', {})
-opts['checkpoint_path'] = MODEL_PATH
-opts = Namespace(**opts)
+    # Define model paths dictionary
+    model_paths = {
+        'psp_model': MODEL_PATH
+    }
 
-# Initialize and load the pSp model
-net = pSp(opts)
-net.eval()
-if torch.cuda.is_available():
-    net.cuda()
-logging.info('Model successfully loaded!')
+    # Check and load model
+    if not os.path.exists(MODEL_PATH):
+        raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
 
-# Define image transformations
-transform = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-])
+    # Load checkpoint
+    ckpt = torch.load(MODEL_PATH, map_location='cpu')  # weights_only 인자 제거
+    opts = ckpt.get('opts', {})
+    opts['checkpoint_path'] = MODEL_PATH
+    opts = Namespace(**opts)
+
+    # Initialize and load the pSp model
+    net = pSp(opts)
+    net.eval()
+    if torch.cuda.is_available():
+        net.cuda()
+    logging.info('Model successfully loaded!')
+
+    # Define image transformations
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+
+class AgeOffloadModelNode:
+    @staticmethod
+    def INPUT_TYPES():
+        return {
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "offload"
+    CATEGORY = "Custom/FaceAging"
+
+    def offload(self):
+        global predictor
+        global net
+        global transform
+        predictor = None
+        net = None
+        transform = None
+
 
 class AgeTransformationNode:
     """
     ComfyUI Custom Node for Age Transformation using pSp.
     """
+
     @staticmethod
     def INPUT_TYPES():
         return {
@@ -84,6 +115,11 @@ class AgeTransformationNode:
     CATEGORY = "Custom/FaceAging"
 
     def transform_age(self, input_image, target_age):
+        global predictor
+        global net
+        global transform
+        if predictor is None or net is None or transform is None:
+            load_model()
         """
         Transforms the input image to the specified target age.
         
@@ -99,7 +135,7 @@ class AgeTransformationNode:
         input_image = input_image.permute(2, 0, 1)
         input_image = input_image.detach().cpu()
         input_image = transforms.ToPILImage()(input_image)
-        
+
         # Align the input image
         aligned_image = align_face(image=input_image, predictor=predictor)
         if aligned_image is None:
@@ -111,18 +147,18 @@ class AgeTransformationNode:
         # Apply age transformation
         age_transformer = AgeTransformer(target_age=target_age)
         with torch.no_grad():
-            input_image_age = age_transformer(input_tensor.cpu()).unsqueeze(0).to('cuda') # add batch dim
+            input_image_age = age_transformer(input_tensor.cpu()).unsqueeze(0).to('cuda')  # add batch dim
             result_tensor = net(input_image_age.float(), randomize_noise=False, resize=False)[0]
             result_image = tensor2im(result_tensor)
             # result_image.save('/workspace/qscar/faceAging/test.png')
 
         # Ensure the output tensor is on CPU and has shape [1, 3, 1024, 1024]
-        result_tensor = torch.from_numpy(np.array(result_image).astype(np.float32) / 255.0).unsqueeze(0) # Add batch dimension if not present
+        result_tensor = torch.from_numpy(np.array(result_image).astype(np.float32) / 255.0).unsqueeze(
+            0)  # Add batch dimension if not present
         result_tensor = result_tensor.cpu()
 
         # logging.info(f'Age transformation successful: {result_tensor.shape}')
-        return (result_tensor, )  # 단일 Tensor 객체 직접 반환
-        
+        return (result_tensor,)  # 단일 Tensor 객체 직접 반환
 
     def UI(self):
         return {
@@ -135,6 +171,8 @@ class AgeTransformationNode:
             }
         }
 
+
 NODE_CLASS_MAPPINGS = {
-    "AgeTransformationNode": AgeTransformationNode
+    "AgeTransformationNode": AgeTransformationNode,
+    "AgeOffloadModelNode": AgeOffloadModelNode
 }
